92095f275c093649e44f9f390bf416f5d25bb2d8
Bakbergen <atymtaevbak@bk.ru>
2022-10-03 21:03:24 +0600

    conf user with in-mem userdetailsmanager

--- src/main/java/com/example/securityb2tech/config/SecurityConfig.java
+++ src/main/java/com/example/securityb2tech/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.example.securityb2tech.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,11 +30,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("bak").password("$2a$12$Fe/aCGSG89oNhoQeiD8aCePZx8vOtg0PMAM1.GqAAySyro2azAKAe").roles("ADMIN")
-                .and()
-                .withUser("aza").password("$2a$12$Fe/aCGSG89oNhoQeiD8aCePZx8vOtg0PMAM1.GqAAySyro2azAKAe").roles("USER")
-                .and()
-                .passwordEncoder(new BCryptPasswordEncoder());
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails user1 = User.withUsername("bak")
+                .password("$2a$12$Fe/aCGSG89oNhoQeiD8aCePZx8vOtg0PMAM1.GqAAySyro2azAKAe")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("aza")
+                .password("$2a$12$Fe/aCGSG89oNhoQeiD8aCePZx8vOtg0PMAM1.GqAAySyro2azAKAe")
+                .roles("USER")
+                .build();
+
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(user2);
+        auth.userDetailsService(userDetailsManager);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
