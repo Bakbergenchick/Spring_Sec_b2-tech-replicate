5da9728a203ac91209f4f8c60ac150a4564a2137
Bakbergen <atymtaevbak@bk.ru>
2022-10-03 20:50:50 +0600

    in-memory auth

--- src/main/java/com/example/securityb2tech/config/SecurityConfig.java
+++ src/main/java/com/example/securityb2tech/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.securityb2tech.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,10 +14,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
                 .and()
+                .httpBasic()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/home").authenticated()
                 .antMatchers("/dashboard").permitAll()
                 .anyRequest().denyAll();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("bak").password("$2a$12$Fe/aCGSG89oNhoQeiD8aCePZx8vOtg0PMAM1.GqAAySyro2azAKAe").roles("ADMIN")
+                .and()
+                .withUser("aza").password("$2a$12$Fe/aCGSG89oNhoQeiD8aCePZx8vOtg0PMAM1.GqAAySyro2azAKAe").roles("USER")
+                .and()
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
